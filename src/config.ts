13beacc11c3276f5fb00d7cf8f1d7d58c22c1175
giuseppecascavilla <g.cascavilla@tue.nl>
2024-05-24 12:29:25 +0200

    config update

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Giuseppe Cascavilla';
+export const SITE_TITLE = 'Giuseppe Cascavilla personal website';
 export const SITE_DESCRIPTION = 'This is a personal website of Giuseppe Cascavilla. Users can find personal information, my research activity, and a bolg where I discuss some scientific (and not) topics.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
