300f42fe1d6fb70eda1ca9f5e4053dbc705b0e52
giuseppecascavilla <g.cascavilla@tue.nl>
2024-05-30 18:53:52 +0200

    sitemap.xml and Robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,17 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   // site: 'https://astrofy-template.netlify.app',
   site: 'https://giuseppecascavilla.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap({
+    filter: (page) => 
+      page !== 'https://giuseppecascavilla.github.io/store/' &&
+      page !== 'https://giuseppecascavilla.github.io/store/item1/' &&
+      page !== 'https://giuseppecascavilla.github.io/store/item2/' &&
+      page !== 'https://giuseppecascavilla.github.io/store/item3/' &&
+      page !== 'https://giuseppecascavilla.github.io/store/item4/' &&
+      page !== 'https://giuseppecascavilla.github.io/store/item5/' &&
+      page !== 'https://giuseppecascavilla.github.io/store/item6/' &&
+      page !== 'https://giuseppecascavilla.github.io/store/item7/' &&
+      page !== 'https://giuseppecascavilla.github.io/store/item8/',
+  }), tailwind()]
+
 });
